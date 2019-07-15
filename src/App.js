44681847e50fd7a27f1e18dd99a6c1809b59374d
itizawa <itizawa@weseek.co.jp>
2019-07-15 12:58:47 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "reactstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Button>aaa</Button>
+    </div>
+  );
+}
+
+export default App;
