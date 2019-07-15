d40259f2e5baa891d043f796fc0ae247b5fdfab0
itizawa <itizawa@weseek.co.jp>
2019-07-15 13:24:18 +0900

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Button } from "reactstrap";
 
+// Containerのインポート
+import Header from "./container/Header"
+
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Button>aaa</Button>
     </div>
   );
