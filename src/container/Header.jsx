02e6029ab08cdaee5386c07f3a534c999df3ced1
itizawa <itizawa@weseek.co.jp>
2019-07-16 12:47:33 +0900

    add modal

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -1,37 +1,88 @@
-import React from 'react';
+import React from "react";
 import {
   Navbar,
   NavbarToggler,
   NavbarBrand,
   Nav,
   NavItem,
-  Button
- } from 'reactstrap';
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from "reactstrap";
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  /**
+   * モーダル開閉のためのイベントハンドラ
+   */
+
+  toggle() {
+    this.setState((prevState) => ({
+      modal: !prevState.modal
+    }));
+  }
 
   render() {
+    const navbar_style = {
+      backgroundColor: "#00075d"
+    };
 
-    const navbar_style={
-     backgroundColor: "#00075d",
-    }
-
-    const text_style={
-     color: "white",
-    }
-
-
+    const text_style = {
+      color: "white"
+    };
 
     return (
       <div>
         <Navbar className="fixed-top" style={navbar_style} expand="md">
-          <NavbarBrand href="/" style={text_style}>Hearth EV</NavbarBrand>
+          <NavbarBrand href="/" style={text_style}>
+            Hearth EV
+          </NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <Button className="bg-primary border border-white rounded-pill py-1 px-5" style={text_style}>Login</Button>
-              </NavItem>
-            </Nav>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <Button
+                onClick={this.toggle}
+                className="bg-primary border border-white rounded-pill py-1 px-5"
+                style={text_style}
+              >
+                Login
+              </Button>
+            </NavItem>
+          </Nav>
+          <Modal
+            isOpen={this.state.modal}
+            toggle={this.toggle}
+            className={this.props.className}
+          >
+            <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+            <ModalBody>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={this.toggle}>
+                Do Something
+              </Button>{" "}
+              <Button color="secondary" onClick={this.toggle}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </Modal>
         </Navbar>
       </div>
     );
