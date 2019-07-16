1de6ecb691235844f1cfe30e3e9ccb4475af45ea
itizawa <itizawa@weseek.co.jp>
2019-07-16 12:40:46 +0900

    get rid of toggle function

--- src/container/Header.jsx
+++ src/container/Header.jsx
@@ -9,19 +9,7 @@ import {
  } from 'reactstrap';
 
 export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
   render() {
 
     const navbar_style={
