50d54a9a35f73d94350c6dbbd181d157f170693d
itizawa <itizawa@weseek.co.jp>
2019-07-17 16:17:20 +0900

    change function into class

--- src/App.js
+++ src/App.js
@@ -5,23 +5,24 @@ import { Row, Col } from "reactstrap";
 import Header from "./container/Header";
 import Sidebar from "./container/Sidebar";
 
-function App() {
+export default class App extends React.Component {
+  render() {
+    const bg_Style = {
+      backgroundColor: "#f6f6f6",
+      position: "fixed",
+      width: "100%",
+      height: "100%"
+    };
 
-  const bg_Style = {
-    backgroundColor: "#f6f6f6",
-    position:"fixed",
-    width:"100%",
-    height: "100%",
-  };
-
-  return (
-    <div style={bg_Style}>
-      <Header />
-      <Row className="mt-5">
-        <Col className="ml-5 mt-3" md="3"><Sidebar /></Col>
-      </Row>
-    </div>
-  );
+    return (
+      <div style={bg_Style}>
+        <Header />
+        <Row className="mt-5">
+          <Col className="ml-5 mt-3" md="3">
+            <Sidebar />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
-
-export default App;
