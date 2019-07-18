1df33bef586ead084d4f5f146d6337a8873f041f
itizawa <itizawa@weseek.co.jp>
2019-07-18 22:52:21 +0900

    success router props

--- src/container/MainContainer.jsx
+++ src/container/MainContainer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Row, Col } from "reactstrap";
 import Sidebar from "./Sidebar";
+import { BrowserRouter, Route } from "react-router-dom";
+import MainApp from "./mainApp/MainApp";
 
 export default class MainContainer extends React.Component {
   render() {
@@ -14,6 +16,15 @@ export default class MainContainer extends React.Component {
           <Col md="3" className="pl-0">
             <Sidebar user_data={this.props.user_data} />
           </Col>
+          <BrowserRouter>
+            <div>
+              <Route
+                exact
+                path="/"
+                render={(props) => <MainApp {...this.props} />}
+              />
+            </div>
+          </BrowserRouter>
           <Col md="5" className="px-0">
             <Sidebar user_data={this.props.user_data} />
           </Col>
--- src/container/mainApp/MainApp.jsx
+++ src/container/mainApp/MainApp.jsx
@@ -2,4 +2,9 @@ import React from "react";
 
 
 export default class MainApp extends React.Component {
+  render(){
+    return(
+      <div>{this.props.user_data.displayName}</div>
+    )
+  }
 }
