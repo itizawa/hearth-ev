4b6a03e9f6e10757a962859103a22c92d9ca7218
itizawa <itizawa@weseek.co.jp>
2019-07-18 20:04:00 +0900

    show img

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -4,12 +4,19 @@ import { Card, CardImg, CardBody, CardTitle } from "reactstrap";
 
 export default class Sidebar extends React.Component {
   render() {
+
+    const photo_style = {
+      float:"right",
+      transform: "translate(10px, -60%)"
+    };
+
     return (
       <Card>
         <CardImg top width="100%" src={logo} alt="Card logo" />
 
         <CardBody>
-          <CardTitle>{this.props.user_data.displayName}</CardTitle>
+          <img  width="40%" style={photo_style} className="rounded-pill border border-secondary" src={this.props.user_data.photoURL} alt="Card logo" />
+          <CardTitle><h2>{this.props.user_data.displayName}</h2></CardTitle>
         </CardBody>
       </Card>
     );
