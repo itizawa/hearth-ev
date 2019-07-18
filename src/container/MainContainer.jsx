cb75fc696a8634b440f405df1076149d2cf0e246
itizawa <itizawa@weseek.co.jp>
2019-07-18 21:03:07 +0900

    adujust

--- src/container/MainContainer.jsx
+++ src/container/MainContainer.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default class MainContainer extends React.Component {
   render() {
-    const container_Style = {
+    const container_style = {
       maxWidth: "1100px",
       backgroundColor: "red"
     };
-    return <div style={container_Style}>sample</div>;
+    return <div style={container_style}>sample</div>;
   }
 }
