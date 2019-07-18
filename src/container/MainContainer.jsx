ca1a42efe4b328d9dd498f7b0b1bbe6b432916ad
itizawa <itizawa@weseek.co.jp>
2019-07-18 21:35:22 +0900

    Adjustment of size

--- src/container/MainContainer.jsx
+++ src/container/MainContainer.jsx
@@ -5,13 +5,19 @@ import Sidebar from "./Sidebar";
 export default class MainContainer extends React.Component {
   render() {
     const container_style = {
-      maxWidth: "1100px",
-      backgroundColor: "red"
+      maxWidth: "1440px",
+      margin: "0 auto"
     };
     return (
       <div style={container_style}>
-        <Row className="mt-5">
-          <Col className="ml-5 mt-3" md="3">
+        <Row className="mt-2 mx-0">
+          <Col md="2" className="px-0">
+            <Sidebar user_data={this.props.user_data} />
+          </Col>
+          <Col md="6" className="px-0">
+            <Sidebar user_data={this.props.user_data} />
+          </Col>
+          <Col md="4" className="pr-0">
             <Sidebar user_data={this.props.user_data} />
           </Col>
         </Row>
