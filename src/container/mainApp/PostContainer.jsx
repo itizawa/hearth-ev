86e5aba5bac942389a93b58db7ba0f8867197aca
itizawa <itizawa@weseek.co.jp>
2019-07-19 23:55:32 +0900

    complete from

--- src/container/mainApp/PostContainer.jsx
+++ src/container/mainApp/PostContainer.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { Input } from "reactstrap";
 
 export default class PostContainer extends React.Component {
-  render(){
-    return(
-      <div>ホーム</div>
-    )
+  render() {
+    const header_style = {
+      backgroundColor: "#00075d"
+    };
+
+    return (
+      <div className="border 2px">
+        <h1 style={header_style} className="text-white py-2 pl-1 mb-0">
+          ホーム
+        </h1>
+        <div className="d-flex py-1">
+          <img
+            className="ml-2 my-auto rounded-pill"
+            src={this.props.user_data.photoURL}
+            alt={this.props.user_data.photoURL}
+            width="35px"
+            height="35px"
+          />
+          <Input className="col-10 ml-2" placeholder="今、何してる" />
+        </div>
+      </div>
+    );
   }
 }
