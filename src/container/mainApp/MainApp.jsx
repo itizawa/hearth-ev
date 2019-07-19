ee4553c363a7b2bc0dc67e8072fec6105b12011b
itizawa <itizawa@weseek.co.jp>
2019-07-19 12:37:46 +0900

    add props

--- src/container/mainApp/MainApp.jsx
+++ src/container/mainApp/MainApp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Row, Col } from "reactstrap";
 import Sidebar from "../Sidebar";
+import PostContainer from "./PostContainer";
 
 export default class MainApp extends React.Component {
   render() {
@@ -8,7 +9,7 @@ export default class MainApp extends React.Component {
       <React.Fragment>
         <Row>
           <Col md="7" className="px-0">
-            <Sidebar user_data={this.props.user_data} />
+            <PostContainer user_data={this.props.user_data} />
           </Col>
           <Col md="5" className="px-0">
             <Sidebar user_data={this.props.user_data} />
---
+++ src/container/mainApp/PostContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default class PostContainer extends React.Component {
+  render(){
+    return(
+      <div>ホーム</div>
+    )
+  }
+}
