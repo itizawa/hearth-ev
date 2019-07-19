d4a088eb5fd1805ed439f81c97b52d46eeeba604
itizawa <itizawa@weseek.co.jp>
2019-07-20 01:46:21 +0900

    adjust col

--- src/container/MainContainer.jsx
+++ src/container/MainContainer.jsx
@@ -13,10 +13,10 @@ export default class MainContainer extends React.Component {
     return (
       <div style={container_style}>
         <Row className="mt-2 mx-0">
-          <Col md="3" className="pl-0">
+          <Col md="2" className="pl-0">
             <Sidebar user_data={this.props.user_data} />
           </Col>
-          <Col md="9">
+          <Col md="10">
             <BrowserRouter>
               <Route
                 exact
