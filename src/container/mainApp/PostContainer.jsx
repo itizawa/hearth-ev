dac3b05390e260a14f4ea736955b22b36fd6e1f4
itizawa <itizawa@weseek.co.jp>
2019-07-22 22:25:52 +0900

    コメント配列

--- src/container/mainApp/PostContainer.jsx
+++ src/container/mainApp/PostContainer.jsx
@@ -10,7 +10,7 @@ export default class PostContainer extends React.Component {
     super(props);
     this.state = {
       show_comment_modal: false,
-      comments: {}
+      comments: []
     };
     this.onReadHandler();
 
