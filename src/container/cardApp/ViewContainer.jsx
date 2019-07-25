ddb473a63f062cae57d7b6c58cf4cfd23bf50f4f
itizawa <itizawa@weseek.co.jp>
2019-07-25 22:59:19 +0900

    card Viewの作成

--- src/container/cardApp/ViewContainer.jsx
+++ src/container/cardApp/ViewContainer.jsx
@@ -1,31 +1,15 @@
 import React from "react";
-import logo from "../../asset/img/logo.png";
 
-import {
-  CardImg,
-  CardBody,
-  Row,
-  Col
-} from "reactstrap";
+import CardView from "./CardView";
 
 export default class Sidebar extends React.Component {
   render() {
-    const row_style={
-      height:"50px"
-    }
+
     return (
       <React.Fragment>
         <div className="border 2px">
-          <CardImg top width="100%" src={logo} alt="Card logo" />
-          <CardBody>
-            <Row className="py-2 mx-0" style={row_style}>
-              <Col xs="8">
-              </Col>
-              <Col xs="4" className="px-1">
+          <CardView />
 
-              </Col>
-            </Row>
-          </CardBody>
         </div>
       </React.Fragment>
     );
