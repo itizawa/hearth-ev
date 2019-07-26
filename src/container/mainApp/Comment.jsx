44020664443c8d78374d41694fca75fba974c859
itizawa <itizawa@weseek.co.jp>
2019-07-26 23:45:15 +0900

    tooltipの設定

--- src/container/mainApp/Comment.jsx
+++ src/container/mainApp/Comment.jsx
@@ -1,15 +1,52 @@
 import React from "react";
-import { Col, Row } from "reactstrap";
+import { Col, Row, Tooltip } from "reactstrap";
 import { Link } from "react-router-dom";
+
+import firebase from "firebase/app";
 
 export default class MainComment extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      comments: this.props.comments
+      comments: this.props.comments,
+      tooltipOpen: false,
+      card_image: ""
     };
+    if (this.props.comment.card_id) {
+      this.fetchCardImage();
+    }
+
+    this.tooltip_toggle = this.tooltip_toggle.bind(this);
   }
+
+  /**
+   * カード画像の取得
+   */
+
+  fetchCardImage() {
+    var storageRef = firebase.storage().ref();
+    var spaceRef = storageRef.child(`card/${this.props.comment.card_id}.png`); //imgとidは兼用
+
+    spaceRef.getDownloadURL().then((url) => {
+      this.setState({ card_image: url });
+    });
+  }
+
+  /**
+   * ツールチップ開閉のためのイベントハンドラ
+   */
+
+  tooltip_toggle() {
+    this.setState({
+      tooltipOpen: !this.state.tooltipOpen
+    });
+  }
+
   render() {
+    const tooltip = {
+      maxWidth: "400px"
+    };
+
     return (
       <Row className="mx-0 py-2 px-2 border-top">
         <Col xs="1" className="px-0">
@@ -36,9 +73,26 @@ export default class MainComment extends React.Component {
           </h5>
           {this.props.comment.card_id && (
             <Link to={"/card/" + this.props.comment.card_id}>
-              <span className="">#{this.props.comment.card_name}</span>
+              <span className="" href="#" id="TooltipExample">
+                #{this.props.comment.card_name}
+              </span>
+              <Tooltip
+                style={tooltip}
+                placement="right"
+                isOpen={this.state.tooltipOpen}
+                target="TooltipExample"
+                toggle={this.tooltip_toggle}
+              >
+                <img
+                  src={this.state.card_image}
+                  alt={this.state.card_image}
+                  width="100%"
+                  height="auto"
+                />
+              </Tooltip>
             </Link>
           )}
+
           <p className="mb-0">{this.props.comment.text}</p>
         </Col>
       </Row>
