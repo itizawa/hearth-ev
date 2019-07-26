30fa71e6481536432f97a4a94020eb1bafc5b225
itizawa <itizawa@weseek.co.jp>
2019-07-26 23:59:45 +0900

    Userコメントにもハッシュタグ表示

--- src/container/userApp/UserComment.jsx
+++ src/container/userApp/UserComment.jsx
@@ -54,14 +54,24 @@ export default class UserComment extends React.Component {
           </Link>
         </Col>
         <Col xs="11" className="px-0">
-          <h5>
+          <h5 className="mb-0">
             <Link to={"/user/" + this.state.comment.creator_id}>
-              <strong className="text-body">{this.state.comment.creator}</strong>
+              <strong className="text-body">
+                {this.state.comment.creator}
+              </strong>
             </Link>
             <small className="text-muted ml-1">
               {this.state.comment.create_at}
             </small>
           </h5>
+          {this.state.comment.card_id && (
+            <Link to={"/card/" + this.state.comment.card_id}>
+              <span className="" href="#" id="TooltipExample">
+                #{this.state.comment.card_name}
+              </span>
+            </Link>
+          )}
+
           <p className="mb-0">{this.state.comment.text}</p>
         </Col>
       </Row>
