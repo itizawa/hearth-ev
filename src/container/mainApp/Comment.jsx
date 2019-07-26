058d8f27d33cb6cd009788ebd517635e4584b5f5
itizawa <itizawa@weseek.co.jp>
2019-07-26 22:26:07 +0900

    comment-component を移動する

--- src/container/mainApp/Comment.jsx
+++ src/container/mainApp/Comment.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Col, Row } from "reactstrap";
 import { Link } from "react-router-dom";
 
-export default class Comment extends React.Component {
+export default class MainComment extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
