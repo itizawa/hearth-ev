ccea0e584ce43e636cf6e9c489d30784f3462299
itizawa <itizawa@weseek.co.jp>
2019-07-27 08:48:12 +0900

    logoの追加

--- src/container/cardList/CenterContainer.jsx
+++ src/container/cardList/CenterContainer.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { CardImg } from "reactstrap";
+
+import 探検同盟 from "../../asset/img/探検同盟.png";
 
 export default class CenterContainer extends React.Component {
   constructor(props) {
@@ -7,7 +10,6 @@ export default class CenterContainer extends React.Component {
       show_comment_modal: false,
       comments: []
     };
-
   }
 
   render() {
@@ -21,6 +23,7 @@ export default class CenterContainer extends React.Component {
           <h3 style={header_style} className="text-white py-2 pl-3 mb-0">
             カードリスト
           </h3>
+          <CardImg top width="100%" src={探検同盟} alt="Card logo" />
           {/* {this.props.focus_card.comments && //commentsが空の時でも動くように
             this.props.focus_card.comments.reverse().map((id, index) => {
               return <CardComment key={index} id={id} />;
