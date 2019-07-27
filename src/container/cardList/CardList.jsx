8b89b86a8adf0782f677dbf25200002906f1e5fd
itizawa <itizawa@weseek.co.jp>
2019-07-27 10:01:52 +0900

    カードデータを受けとる

--- src/container/cardList/CardList.jsx
+++ src/container/cardList/CardList.jsx
@@ -1,10 +1,46 @@
 import React from "react";
 
+import firebase from "firebase/app";
+
 export default class CardList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: []
+    };
+
+    this.fetchHeroComment();
+  }
+
+  /**
+   * データを取得するイベントハンドラ
+   */
+
+  fetchHeroComment = function() {
+    var cards = [];
+    const db = firebase.firestore();
+    db.collection("Cards")
+      .where("hero", "==", "druid")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          cards.push(doc.data());
+        });
+        this.setState({ cards: cards });
+      })
+      .catch(function(error) {
+        console.log("Error getting documents: ", error);
+      });
+
+    return Promise.all([db]);
+  };
+
   render() {
     return (
       <React.Fragment>
-        <div>{this.props.hero}</div>
+        {this.state.cards.map((card, index) => {
+          return <div key={index}>{card.name}</div>;
+        })}
       </React.Fragment>
     );
   }
