12f989c8e675647c698fa79daf78726ddfd92d09
itizawa <itizawa@weseek.co.jp>
2019-07-27 10:55:31 +0900

    class追加

--- src/container/cardList/CenterContainer.jsx
+++ src/container/cardList/CenterContainer.jsx
@@ -19,8 +19,8 @@ export default class CenterContainer extends React.Component {
     super(props);
     this.state = {
       activeTab: 0,
-      hero: ["druid", "mage", "hunter"],
-      hero_J: ["ドルイド", "メイジ", "ハンター"]
+      hero: ["druid", "hunter", "mage", "paladin", "priest","rogue","sharman","warlock","warrior","neutral"],
+      hero_J: ["ドルイド", "ハンター", "メイジ", "パラディン", "プリースト","ローグ", "シャーマン", "ウォーロック", "ウォリアー", "中立"],
     };
 
     this.toggle = this.toggle.bind(this);
@@ -51,7 +51,7 @@ export default class CenterContainer extends React.Component {
             カードリスト
           </h3>
           <CardImg top width="100%" src={探検同盟} alt="Card logo" />
-          <Nav tabs className="pl-2 mt-3">
+          <Nav tabs className="pl-3 mt-3">
             {this.state.hero_J.map((hero, index) => {
               return (
                 <NavItem key={index}>
