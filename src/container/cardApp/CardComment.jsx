613acd34d30e18c97109caba287a6b1b195cf729
itizawa <itizawa@weseek.co.jp>
2019-07-28 13:38:35 +0900

    CardComment

--- src/container/cardApp/CardComment.jsx
+++ src/container/cardApp/CardComment.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Col, Row } from "reactstrap";
 import { Link } from "react-router-dom";
 
@@ -67,4 +68,8 @@ export default class CardComment extends React.Component {
       </Row>
     );
   }
+}
+
+CardComment.propsType={
+  id:PropTypes.string
 }
