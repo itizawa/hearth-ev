cfeda7340a1222eeeea2f4da875eac764a382f01
itizawa <itizawa@weseek.co.jp>
2019-07-31 15:07:53 +0900

    like_countの反映

--- src/container/mainApp/MainComment.jsx
+++ src/container/mainApp/MainComment.jsx
@@ -74,6 +74,7 @@ export default class MainComment extends React.Component {
         like: firebase.firestore.FieldValue.arrayUnion(this.props.user_data.uid)
       });
     this.setState({ isLiked: true });
+    this.setState({ like_count: this.state.like_count+1})
   }
 
   /**
@@ -90,6 +91,7 @@ export default class MainComment extends React.Component {
         )
       });
     this.setState({ isLiked: false });
+    this.setState({ like_count: this.state.like_count-1})
   }
 
   /**
