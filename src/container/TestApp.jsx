1ad0ac03a18bc2e979544c21440e480eb4832326
itizawa <itizawa@weseek.co.jp>
2019-08-04 03:40:04 +0900

    buttonの配置

--- src/container/TestApp.jsx
+++ src/container/TestApp.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Button } from "reactstrap";
 
 export default class TestApp extends React.Component {
+  
+  /**
+   * コメント投稿のイベントハンドラ
+   */
+
+  onPostComment() {
+    console.log("hello");
+  }
+
   render() {
-    return <div>HelloTest</div>;
+    return (
+      <Button color="primary" onClick={this.onPostComment}>
+        Add
+      </Button>
+    );
   }
 }
