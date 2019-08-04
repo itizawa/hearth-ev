951c126d3b088c45fde064b058db4603bea36e6a
itizawa <itizawa@weseek.co.jp>
2019-08-04 09:04:58 +0900

    bodyにbgカラーを設定

--- src/App.js
+++ src/App.js
@@ -38,17 +38,11 @@ export default class App extends React.Component {
   }
 
   render() {
-    const bgStyle = {
-      backgroundColor: "#e6ecf0",
-      position: "fixed",
-      height: "100%",
-      width: "100%"
-    };
     return (
-      <div style={bgStyle}>
+      <React.Fragment>
         <Header user_data={this.state.user_data} />
         <MainContainer user_data={this.state.user_data} />
-      </div>
+      </React.Fragment>
     );
   }
 }
