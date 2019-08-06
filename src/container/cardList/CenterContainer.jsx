3560640a82d70f6e1e64ab772498a1562169ae7a
itizawa <itizawa@weseek.co.jp>
2019-08-06 21:24:32 +0900

    Card Listの調整

--- src/container/cardList/CenterContainer.jsx
+++ src/container/cardList/CenterContainer.jsx
@@ -57,7 +57,7 @@ export default class CenterContainer extends React.Component {
       <React.Fragment>
         <div className="bg-white border 2px shadow-sm">
           <h3 style={header_style} className="text-white py-2 pl-3 mb-0">
-            カードリスト
+            Card List
           </h3>
           <CardImg top width="100%" src={探検同盟} alt="Card logo" />
           <Nav tabs className="pl-3 mt-3">
