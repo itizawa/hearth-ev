02da1fcaaf853f32b33694770a550e4ce370d064
itizawa <itizawa@weseek.co.jp>
2019-08-12 04:25:34 +0900

    マウスオーバーした時にボタンを表示する

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -16,7 +16,8 @@ export default class Comment extends React.Component {
       comments: this.props.comments,
       tooltipOpen: false,
       isLiked: this.fetchIsLiked(),
-      like_count: this.props.comment.like.length
+      like_count: this.props.comment.like.length,
+      show_operation_button: false
     };
 
     this.tooltip_toggle = this.tooltip_toggle.bind(this);
@@ -87,6 +88,17 @@ export default class Comment extends React.Component {
   }
 
   /**
+   * マウスオーバーした時のイベントハンドラ
+   */
+  onMouseOver() {
+    this.setState({ show_operation_button: true });
+  }
+
+  onMouseLeave() {
+    this.setState({ show_operation_button: false });
+  }
+
+  /**
    * いいねボタンを取り消したときのイベントハンドラ
    */
 
@@ -121,7 +133,15 @@ export default class Comment extends React.Component {
 
     return (
       <React.Fragment>
-        <Row className="mx-0 py-2 px-2 border-top">
+        <Row
+          className="mx-0 py-2 px-2 border-top"
+          onMouseOver={() => {
+            this.onMouseOver();
+          }}
+          onMouseLeave={() => {
+            this.onMouseLeave();
+          }}
+        >
           <Col xs="1" className="px-0">
             <Link to={"/user/" + comment.creator_id + "/timestamp"}>
               <img
@@ -144,14 +164,24 @@ export default class Comment extends React.Component {
                 className="text-muted float-right"
                 onClick={this.delete_modal_toggle}
               >
-                <i className="material-icons btn p-0">clear</i>
+                <i
+                  hidden={!this.state.show_operation_button}
+                  className="material-icons btn p-0"
+                >
+                  clear
+                </i>
               </span>
               <span
                 hidden={comment.creator_id !== user_data.uid}
                 className="text-muted float-right"
                 onClick={this.reEdit_modal_toggle}
               >
-                <i className="material-icons btn p-0 mr-2">edit</i>
+                <i
+                  hidden={!this.state.show_operation_button}
+                  className="material-icons btn p-0 mr-2"
+                >
+                  edit
+                </i>
               </span>
             </h5>
             {comment.card_id && (
