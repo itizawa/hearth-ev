1824bea8452368c3a33595666029b7170439707f
itizawa <itizawa@weseek.co.jp>
2019-08-15 18:03:00 +0900

    ページの作成

--- src/container/topicApp/CenterContainer.jsx
+++ src/container/topicApp/CenterContainer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Input, Col, Row } from "reactstrap";
 
+import CommentModal from "../../components/CommentModal";
 import Comment from "../../components/Comment";
 
 import firebase from "firebase/app";
@@ -13,17 +15,28 @@ export default class CenterContainer extends React.Component {
       comments: []
     };
 
-    // this.fetchUserComment(this.props.focus_user.id);
+    this.fetchTopicComment(this.props.focus_topic.id);
+
+    this.modal_toggle = this.modal_toggle.bind(this);
+  }
+
+  /**
+   * モーダル開閉のためのイベントハンドラ
+   */
+  modal_toggle() {
+    this.setState((prevState) => ({
+      show_comment_modal: !prevState.show_comment_modal
+    }));
   }
 
   /**
    * コメントデータを取得する
    */
-  fetchUserComment = (user_id = this.props.focus_user.uid) => {
+  fetchTopicComment = (target = this.props.focus_topic.topic_id) => {
     var comments = [];
     const db = firebase.firestore();
     db.collection("Comments")
-      .where("creator_id", "==", user_id)
+      .where("topic_id", "==", target)
       .orderBy("timestamp", "desc")
       .get()
       .then((querySnapshot) => {
@@ -43,13 +56,15 @@ export default class CenterContainer extends React.Component {
       backgroundColor: "#00075d"
     };
 
+    const { focus_topic, user_data } = this.props;
+
     const comment = this.state.comments.map((comment) => {
       return (
         <Comment
           key={comment.comment_id}
           comment={comment}
-          user_data={this.props.user_data}
-          fetchComment={this.fetchUserComment}
+          user_data={user_data}
+          fetchComment={this.fetchTopicComment}
         />
       );
     });
@@ -58,10 +73,34 @@ export default class CenterContainer extends React.Component {
       <React.Fragment>
         <div className="bg-white border 2px shadow-sm">
           <h3 style={header_style} className="text-white py-2 pl-3 mb-0">
-            Topic Page
+            Topic Page :{this.props.focus_topic.topic_name}
           </h3>
+          <Row className="py-2 mx-0">
+            <Col xs="1" className="px-1">
+              <img
+                className="rounded-pill"
+                src={user_data.photoURL}
+                alt={user_data.photoURL}
+                width="80%"
+                height="auto"
+              />
+            </Col>
+            <Col xs="11 pl-0">
+              <Input onClick={this.modal_toggle} placeholder="コメントする" />
+            </Col>
+          </Row>
           {comment}
         </div>
+
+        <CommentModal
+          modal={this.state.show_comment_modal}
+          modal_toggle={this.modal_toggle}
+          user_data={user_data}
+          card_id={focus_topic.card_id}
+          card_name={focus_topic.card_name}
+          pushCommentId={this.props.pushCommentId}
+          fetchComment={this.fetchTopicComment}
+        />
       </React.Fragment>
     );
   }
@@ -69,5 +108,5 @@ export default class CenterContainer extends React.Component {
 
 CenterContainer.propTypes = {
   user_data: PropTypes.object,
-  focus_user: PropTypes.object
+  focus_topic: PropTypes.object
 };
