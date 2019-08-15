27041d50656290bf892dc5d433e9d62c6656fc13
itizawa <itizawa@weseek.co.jp>
2019-08-15 20:21:26 +0900

    作成

--- src/components/TopicListItem.jsx
+++ src/components/TopicListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Row, Col, Tooltip } from "reactstrap";
 import { Link } from "react-router-dom";
 
-export default class UserListItem extends React.Component {
+export default class TopicListItem extends React.Component {
   constructor(props) {
     super(props);
 
@@ -21,38 +21,33 @@ export default class UserListItem extends React.Component {
   }
 
   render() {
-    const { user_data } = this.props;
+    const chat_style = {
+      fontSize: "24px"
+    };
+
+    const { topic_data } = this.props;
     return (
       <Row className="mx-0 py-2 px-2 border-top">
-        <Col xs="2" className="px-0">
-          <Link to={"/user/" + user_data.uid + "/timestamp"}>
-            <img
-              className="rounded-pill border"
-              src={user_data.photoURL}
-              alt={user_data.photoURL}
-              width="80%"
-              height="auto"
-            />
-          </Link>
-        </Col>
         <Col xs="10" className="px-0">
           <h5 className="mb-0">
-            <Link to={"/user/" + user_data.uid + "/timestamp"}>
-              <strong className="text-body">{user_data.name}</strong>
+            <Link to={"/topic/" + topic_data.topic_id}>
+              <strong className="text-body">{topic_data.topic_name}</strong>
+              <span href="#" id={topic_data.topic_id}>
+                <i className="material-icons ml-1" style={chat_style}>
+                  chat
+                </i>
+                {topic_data.comments}
+              </span>
             </Link>
             <p className="my-1">
-              <span className="text-primary" href="#" id={user_data.uid}>
-                <i className="material-icons">grade</i>
-                {user_data.acquired}
-              </span>
               <Tooltip
                 placement="bottom"
                 isOpen={this.state.tooltipOpen}
                 autohide={false}
-                target={user_data.uid}
+                target={topic_data.topic_id}
                 toggle={this.toggle}
               >
-                いいね獲得数
+                コメント総数
               </Tooltip>
             </p>
           </h5>
--- src/container/mainApp/ViewContainer.jsx
+++ src/container/mainApp/ViewContainer.jsx
@@ -2,20 +2,23 @@ import React from "react";
 
 import firebase from "firebase/app";
 import UserListItem from "../../components/UserListItem";
+import TopicListItem from "../../components/TopicListItem";
 
 export default class ViewContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      users: []
+      users: [],
+      topics: []
     };
+
     this.fetchUserData();
+    this.fetchTopicData();
   }
 
   /**
    * ユーザーデータを取得するイベントハンドラ
    */
-
   fetchUserData = () => {
     var users = [];
     const db = firebase.firestore();
@@ -35,13 +38,35 @@ export default class ViewContainer extends React.Component {
     return Promise.all([db]);
   };
 
+  /**
+   * トピックデータを取得するイベントハンドラ
+   */
+  fetchTopicData = () => {
+    var topics = [];
+    const db = firebase.firestore();
+    db.collection("Topics")
+      .orderBy("comments", "desc")
+      .limit(3)
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          topics.push(doc.data());
+        });
+        this.setState({ topics: topics });
+      })
+      .catch((err) => {
+        console.log("Error getting documents", err);
+      });
+    return Promise.all([db]);
+  };
+
   render() {
     const header_style = {
       backgroundColor: "#00075d"
     };
 
     const trending_style = {
-      fontSize: "40px"
+      fontSize: "33px"
     };
 
     return (
@@ -55,6 +80,18 @@ export default class ViewContainer extends React.Component {
           </h3>
           {this.state.users.map((user, index) => {
             return <UserListItem key={index} user_data={user} />;
+          })}
+        </div>
+
+        <div className="bg-white border 2px shadow-sm mt-2">
+          <h3 style={header_style} className="text-white py-2 pl-3 mb-0">
+            <i className="material-icons mr-2" style={trending_style}>
+              trending_up
+            </i>
+            HotTopic
+          </h3>
+          {this.state.topics.map((topic, index) => {
+            return <TopicListItem key={index} topic_data={topic} />;
           })}
         </div>
       </React.Fragment>
