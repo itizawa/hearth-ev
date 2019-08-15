54e29fd3a2b5a498768876d9cbc19eac01c52a5e
itizawa <itizawa@weseek.co.jp>
2019-08-15 15:24:50 +0900

    TopicApp の作成

---
+++ src/container/topicApp/TopicrApp.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Row, Col } from "reactstrap";
+import ViewContainer from "./ViewContainer";
+import CenterContainer from "./CenterContainer";
+
+import firebase from "firebase/app";
+
+export default class TopicApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      focus_user: {
+        id: this.props.match.params.user_id,
+        name: "TopicPage",
+        comments: []
+      }
+    };
+
+    // this.fetchTopicData();
+  }
+
+  /**
+   * ユーザーデータを取得する
+   */
+  fetchTopicData = async () => {
+    await firebase
+      .firestore()
+      .collection("Users")
+      .doc(this.state.focus_user.id)
+      .get()
+      .then((doc) => {
+        if (!doc.exists) {
+          console.log("No such document!");
+        } else {
+          this.setState({
+            focus_user: doc.data()
+          });
+        }
+      })
+      .catch((err) => {
+        console.log("Error getting document", err);
+      });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>this is topic page</div>
+        {/* <Row>
+          <Col md="7" className="px-0 mb-2">
+            <CenterContainer
+              {...this.props}
+              focus_user={this.state.focus_user}
+              match={this.props.match.params.user_id}
+            />
+          </Col>
+          <Col md="5" className="px-1">
+            <ViewContainer
+              user_data={this.props.user_data}
+              focus_user={this.state.focus_user}
+            />
+          </Col>
+        </Row> */}
+      </React.Fragment>
+    );
+  }
+}
