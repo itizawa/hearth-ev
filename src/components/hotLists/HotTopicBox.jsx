7e90a14c2d2425635c1a90e2b5f24c1c94cbcf93
itizawa <itizawa@weseek.co.jp>
2019-08-28 16:14:23 +0900

    adjust

--- src/components/hotLists/HotTopicBox.jsx
+++ src/components/hotLists/HotTopicBox.jsx
@@ -1,18 +1,17 @@
-import React from "react";
+import React from 'react'
 
-import TopicListItem from "../listItems/TopicListItem";
+import TopicListItem from '../listItems/TopicListItem'
 
 // functionのインポート
 import { fetchHotTopicData } from '../../function/topic'
 
 export default class HotTopicBox extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       topics: []
-    };
-    this.fetchTopicData();
-
+    }
+    this.fetchTopicData()
   }
 
   /**
@@ -23,27 +22,27 @@ export default class HotTopicBox extends React.Component {
     console.log(data)
   };
 
-  render() {
+  render () {
     const header_style = {
-      backgroundColor: "#00075d"
-    };
+      backgroundColor: '#00075d'
+    }
 
     const trending_style = {
-      fontSize: "33px"
-    };
+      fontSize: '33px'
+    }
 
     return (
-      <div className="bg-white border 2px shadow-sm mb-2">
-        <h3 style={header_style} className="text-white py-2 pl-3 mb-0">
-          <i className="material-icons mr-2" style={trending_style}>
+      <div className='bg-white border 2px shadow-sm mb-2'>
+        <h3 style={header_style} className='text-white py-2 pl-3 mb-0'>
+          <i className='material-icons mr-2' style={trending_style}>
             trending_up
           </i>
           HotTopic
         </h3>
         {this.state.topics.map((topic, index) => {
-          return <TopicListItem key={index} topic_data={topic} />;
+          return <TopicListItem key={index} topic_data={topic} />
         })}
       </div>
-    );
+    )
   }
 }
