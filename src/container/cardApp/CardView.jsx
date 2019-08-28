374cedd4b117cd8b4e031d64b9c8f19fa8f60cec
itizawa <itizawa@weseek.co.jp>
2019-08-29 01:20:13 +0900

    backgroundImageの設定

--- src/container/cardApp/CardView.jsx
+++ src/container/cardApp/CardView.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import BgImage from '../../asset/img/bg.jpg'
+
 export default class CardView extends React.Component {
   constructor (props) {
     super(props)
@@ -11,15 +13,15 @@ export default class CardView extends React.Component {
 
   render () {
     const img_frame = {
-      height: '700px',
-      padding: '5% 10%'
+      padding: '5%',
+      backgroundImage: `url(${BgImage})`
     }
     return (
       <>
         <div style={img_frame}>
           <img
             src={this.props.focus_card.card_img}
-            alt={this.props.focus_card.card_img}
+            alt={this.props.focus_card.card_id}
             width='100%'
             height='auto'
           />
