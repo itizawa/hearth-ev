c35797056a7e5b6f030613fc9c7d93fc7b32c1ba
itizawa <itizawa@weseek.co.jp>
2019-08-29 02:16:11 +0900

    paddingの設定

--- src/components/listItems/UserListItem.jsx
+++ src/components/listItems/UserListItem.jsx
@@ -6,7 +6,7 @@ export default class UserListItem extends React.Component {
   render () {
     const { user_data } = this.props
     return (
-      <Row className='mx-0 py-2 px-2 border-top'>
+      <Row className='mx-0 p-2 border-top'>
         <Col xs='2' className='px-0'>
           <Link to={'/user/' + user_data.uid + '/timestamp'}>
             <img
