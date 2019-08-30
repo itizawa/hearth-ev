5cce8ec54bba33216df07a0222adcb25fb5f711a
itizawa <itizawa@weseek.co.jp>
2019-08-30 21:28:54 +0900

    adjust

--- src/components/ReEditModal.jsx
+++ src/components/ReEditModal.jsx
@@ -2,17 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {
   Button,
-  DropdownItem,
-  DropdownToggle,
-  DropdownMenu,
   Input,
-  InputGroup,
   Modal,
   ModalHeader,
   ModalBody,
   ModalFooter,
   Row,
-  UncontrolledDropdown,
   Col
 } from 'reactstrap'
 
