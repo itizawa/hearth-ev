3677e66a948dc229a4486748c10ea7940419ace2
itizawa <itizawa@weseek.co.jp>
2019-08-31 12:39:29 +0900

    名前の修正

--- src/container/topicList/ViewContainer.jsx
+++ src/container/topicList/ViewContainer.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 
-import HotUserBox from '../../components/hotLists/HotUserBox'
-import HotCardBox from '../../components/hotLists/HotCardBox'
-
 export default class ViewContainer extends React.Component {
   render () {
     return (
       <>
-        <HotUserBox />
-        <HotCardBox />
+
       </>
     )
   }
