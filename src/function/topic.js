d2e54759dbac21396be172a162c765da0d9b6ee4
itizawa <itizawa@weseek.co.jp>
2019-08-31 14:57:49 +0900

    topicを登録する

--- src/function/topic.js
+++ src/function/topic.js
@@ -9,12 +9,18 @@ import getNow from './getNow'
 /**
  * topic作成ボタンを押した時の処理
  */
-export const createNewTopic = (topicText) => {
-  const data = {
+export const createNewTopic = async (topicText) => {
+
+  const ref = await firebase.firestore().collection('Topics').add({
     topic_name: topicText,
     comments: 0,
     update_at: getNow(),
     timestamp: firebase.firestore.FieldValue.serverTimestamp()
-  }
-  console.log(data)
+  })
+
+  // IDを登録する
+  await firebase.firestore().collection('Topics')
+    .doc(ref.id)
+    .set({ topic_id: ref.id }, { merge: true })
+
 }
