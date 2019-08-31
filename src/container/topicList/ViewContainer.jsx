7b13f524ea41c5c724c19484341ef22365ab7a0c
itizawa <itizawa@weseek.co.jp>
2019-08-31 15:02:14 +0900

    とりあえずviewContainerに表示

--- src/container/topicList/ViewContainer.jsx
+++ src/container/topicList/ViewContainer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import HotUserBox from '../../components/hotBoxes/HotUserBox'
+import HotCardBox from '../../components/hotBoxes/HotCardBox'
+
 export default class ViewContainer extends React.Component {
   render () {
     return (
       <>
-
+        <HotUserBox />
+        <HotCardBox />
       </>
     )
   }
